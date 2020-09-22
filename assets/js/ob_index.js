57b8dc40ca8e411c5dcc1b0749b9f00ad454c884
obisims <33389230+obisims@users.noreply.github.com>
2020-09-23 08:13:19 +1000

    idk
    a bunch of stuff since last time, opt, loader for iframe.. doing mobnav animations now

--- assets/js/ob_index.js
+++ assets/js/ob_index.js
@@ -22,12 +22,15 @@ function shrinkDeadSpace(toShrink){
     console.log('[shrinkDeadSpace] shrinking...')
         $('#landing').addClass('shrunk')//98vh
         $('.shrinkCompen').addClass('unshrunk')
-        var $intro = $('#intro')
-        $intro.addClass('invoiceOpen')
+
+        //$('.shrinkCompen.shrinkCompen_top.unshrunk').css('height','')
+
+
+        if(stateSettings.status.iFrameLoaded==true)$('#intro').addClass('invoiceOpen')
+        
         //$('#navAndMain').removeClass('whiteBG')
         $('#pdfViewer').addClass('invoiceOpen')
-
-        
+        $('#navAndMain').addClass('invoiceOpen')
 
         /*ANIMATE BACKTO TOP ARROWS */
         console.log('[shrinkDeadSpace][InvoiceUI]','setTimeout')
@@ -35,13 +38,14 @@ function shrinkDeadSpace(toShrink){
            //stateSettings.status.deadSpace = 'shrunk' 
             var uiSettings = {
                 deadSpace_state_now:stateSettings.status.deadSpace,
-                isMobile:isMobile
+                isMobile:stateSettings.status.isMobile
             }
         console.log('[shrinkDeadSpace][InvoiceUI] uiSettings',uiSettings) 
             if(uiSettings.deadSpace_state_now=='shrunk'&&uiSettings.isMobile==true){
             console.log('[shrinkDeadSpace][InvoiceUI] Invoice Mobile UI loading...') 
+            
                 $('.mobileNavButton').addClass('show')
-                animateCSS('.mobileNavButton', 'fadeInLeft').then((message) => {
+                animateCSS('.mobileNavButton', 'fadeInLeft',null,null,'animateCSSHide').then((message) => {
                     // Do something after the animation
                     console.log('[shrinkDeadSpace][InvoiceUI] Invoice Mobile Nav Bar loaded') //$('.mobileNavButton').show()
                 })
@@ -77,6 +81,21 @@ function shrinkDeadSpace(toShrink){
             console.log('[shrinkDeadSpace][iFrame][load] not loaded!, loading...',iFrameSrc)
                 $iframe.attr("src",iFrameSrc);
                 stateSettings.status.iFrameLoaded = true;
+                    var iframe = document.getElementById('pdfIframe')
+                    if (navigator.userAgent.indexOf("MSIE") > -1 && !window.opera) {
+                        
+                        iframe.onreadystatechange = function(){
+                        if (iframe.readyState == "complete"){
+                            $('#binderCover').remove()
+                            $('#intro').addClass('invoiceOpen')
+                        }
+                        };
+                    } else {
+                        iframe.onload = function(){
+                            $('#binderCover').remove()
+                            $('#intro').addClass('invoiceOpen')
+                        };
+                    }
                 console.log('[shrinkDeadSpace][iFrame][load] iFrame Loaded for first time:',true)
             }else{
                 console.log('[shrinkDeadSpace][iFrame][load] iFrame already loaded')
@@ -96,14 +115,15 @@ function shrinkDeadSpace(toShrink){
         
         $('#pdfViewer').removeClass('invoiceOpen')
         $('#intro').removeClass('invoiceOpen')
-
+        $('#navAndMain').removeClass('invoiceOpen')
         
         /* LOAD OUT UI */        
         if(isMobile){
-            animateCSS('.mobileNavButton', 'fadeOutLeft').then((message) => {
+            animateCSS('.mobileNavButton', 'fadeOutLeft',null,null,'animateCSSHide').then((message) => {
+                $('.mobileNavButton').removeClass('show')
                 // Do something after the animation
                 console.log('mobileNav faded out')
-                $('.mobileNavButton').removeClass('show')
+              //  $('.mobileNavButton').hide()
             })
         }else{
             $('.arrowUp').removeClass('invoiceOpen')
@@ -111,7 +131,7 @@ function shrinkDeadSpace(toShrink){
         
         $('#duotone').removeClass('shrunkMode')
 
-        animateCSS('iframe', 'slideOutDown','fasts').then((message) => {
+        animateCSS('iframe', 'slideOutDown').then((message) => {
             // Do something after the animation
             console.log('iframe backOutDown')
             if(isMobile==true&&deadSpaceShrunk==false)window.scrollTo(0,1)
@@ -242,7 +262,7 @@ $(document).on( 'scroll', function(){
         main:{vh:0.83},//93vh on desktop main intro
         shrinkCompenB:{vh:0.035},
         window:{vh:1,px:windowHeight},
-        screen:{vh:screenHeight/windowHeight,px:screenHeight}
+        screen:{vh:screenHeight/windowHeight,px:screenHeight,sh:1}
     }
     
     /*function vhConvert(vh,converter){
@@ -259,7 +279,7 @@ $(document).on( 'scroll', function(){
     elemHeights.shrinkCompenA.sh = elemHeights.shrinkCompenA.px/screenHeight
     elemHeights.main.sh = elemHeights.main.px/screenHeight
     elemHeights.shrinkCompenB.sh = elemHeights.shrinkCompenB.px/screenHeight
-    
+    elemHeights.window.sh = elemHeights.window.px/screenHeight
 
     if(!isMobile){
         // for desktop
@@ -302,19 +322,43 @@ $(document).on( 'scroll', function(){
 
         ///OH!!! fucking overscroll!, so we can just use minueses?
         vhInPixels_expandAt = -1
+        //Fuck, my android doesn't support and it's forcing me to refresh wehen i scroll up
+        //i redesigned a bunch of the hight systems and stuff, lets see if that actully fixed what was up here and try my original intuitions.
+        //vhInPixels_expandAt =  0 //shrinkToHeight - (shrinkToHeight*ExpandStuffAt);
 
     }
     //if(vhInPixels_expandAt<=0)vhInPixels_expandAt = shrinkToHeight*0.1
     vhInPixels_shrinkAt = (windowHeight * ShrinkStuffAt);
-    var sortingOutHeights = {
-        window:windowHeight,
-        screenHeight:screenHeight,
-        shrinkToHeight:shrinkToHeight,
-        ExpandStuffAt:ExpandStuffAt,
-        ShrinkStuffAt:ShrinkStuffAt,
-        expandAt:vhInPixels_expandAt,
-        shrinkAt:vhInPixels_shrinkAt
+    
+    var elemHeights = {/*vh and px vals 0.5 = 50vh */
+        shrunkSpacer:{px:50},
+        shrinkCompenA:{vh:0.035},
+        main:{vh:0.83},//93vh on desktop main intro
+        shrinkCompenB:{vh:0.035},
+        window:{vh:1,px:windowHeight},
+        screen:{vh:screenHeight/windowHeight,px:screenHeight}
     }
+    var elemHeights = getScreenHeights()
+    
+
+    var sortingOutHeights = {
+        window:elemHeights.window.px,
+        screenHeight:elemHeights.screen.px,
+        shrinkToHeight:elemHeights.shrunkSpacer.px,
+        ExpandStuffAt:gateWaySettings.elements.deadSpace.expandAt,
+        ShrinkStuffAt:gateWaySettings.elements.deadSpace.shrinkAt,
+        expandAt:vhInPixels_expandAt,
+        shrinkAt:(windowHeight * ShrinkStuffAt)
+    }
+
+    var scrollThresholds = {
+        ExpandStuffAt:gateWaySettings.elements.deadSpace.expandAt,
+        ShrinkStuffAt:gateWaySettings.elements.deadSpace.shrinkAt,
+        expandAt:vhInPixels_expandAt,
+        shrinkAt:(windowHeight * ShrinkStuffAt)
+    }
+   
+    
     //console.log('[onScroll] sortingOutHeights',sortingOutHeights)
     var scrollster = {
         //scroll:scroll,
@@ -323,15 +367,9 @@ $(document).on( 'scroll', function(){
         expandAt:vhInPixels_expandAt,
         //shrinkToHeight:shrinkToHeight
     }
+   
 
     console.log('[scroll]',scroll,{stateSettings:stateSettings,scrollster:scrollster,elemHeights:elemHeights})
-
-    //var heightser = {
-    //	innerHeight:innerHeight,
-    //	windowHeight:windowHeight
-    //}
-    //console.log('[scroll] heightser: ',heightser);
-    //actionAtScroll = 150 // 250
     
     if(scroll>sortingOutHeights.shrinkAt&&stateSettings.status.deadSpace == 'full'){
         console.log('[scroll] shrinking: ',scroll,{stateSettings:stateSettings,scrollster:scrollster});
@@ -352,6 +390,7 @@ $(document).on( 'scroll', function(){
         //stateSettings.status.deadSpace = 'full'
        // console.log('[!GLOBAL SET!][scroll]','deadSpaceShrunk:',deadSpaceShrunk)
     }
+
 });
 
 
@@ -376,7 +415,8 @@ $(document).ready(function() {
         //(elemHeights.screen.vh*7)+50
         $('#intro').css('height','83vh')
         $('#shrinkMobileBuffer').css('height',(window.screen.height/$(window).height()*7))
-        window.scrollTo(0,1)
+        //window.scrollTo(0,1)
+        scrollTo(1, 200);
     }
 
     /////////////////////
@@ -388,12 +428,14 @@ $(document).ready(function() {
 	$( "#mobile_scrollToTop_UP" ).click(function() {
 		console.log('[clicked] mobile arrowUp')
         shrinkDeadSpace(false)
-        scrollToTop()
+       // scrollToTop()
+        scrollTo(1, 400);
 	});
 	$( ".arrowUp" ).click(function() {
 		console.log('[clicked] arrowUp')
 		shrinkDeadSpace(false)
-		scrollToTop()
+        //scrollToTop()
+        scrollTo(1, 400);
 		/*
 		window.scrollTo({
 			top: 0,
@@ -403,13 +445,22 @@ $(document).ready(function() {
 		*/
 	});
 	$( ".arrow" ).click(function() {
-		console.log('[clicked] arrowDown')
-		shrinkDeadSpace(true)
-		window.scrollTo({
+        console.log('[clicked] arrowDown')
+        var nowHeights = getScreenHeights()
+        var scrollingTo = nowHeights.shrinkCompenA.px+nowHeights.shrunkSpacer.px// document.body.scrollHeight
+        console.log('[clicked] scrolling down to',scrollingTo)
+        scrollTo(scrollingTo, 400);
+        /*window.scrollTo({
+			top: dscrollingTo,
+			left: 0,
+			behavior: 'smooth'
+		});*/
+        shrinkDeadSpace(true)
+		/*window.scrollTo({
 			top: document.body.scrollHeight,
 			left: 0,
 			behavior: 'smooth'
-		});
+		});*/
 		//  window.scrollTo(0,);
     });
     /////////////////////
@@ -488,6 +539,7 @@ var gateWaySettings = {
         }
     }
    // stateSettings.status.deadSpace = 'shrunk' // 'full'
+   
 var stateSettings={
     status:{
         scroll:0,
@@ -524,6 +576,7 @@ var isMobile = stateSettings.status.isMobile || false; //initiate as false
 if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|ipad|iris|kindle|Android|Silk|lge |maemo|midp|mmp|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i.test(navigator.userAgent) 
     || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(navigator.userAgent.substr(0,4))) { 
     isMobile = true;
+    stateSettings.status.isMobile = true
 }
 
 
--- assets/js/ob_utils.js
+++ assets/js/ob_utils.js
@@ -21,25 +21,24 @@ const scrollToTop = () => {
 */////////////////////////////////////////
 var animationEnd = 'webkitAnimationEnd mozAnimationEnd oanimationend animationend';
 
-const animateCSS = (element, animation,speed, prefix = 'animate__') =>
+const animateCSS = (element, animation,speed, addExtraClass,removeExtraClass,prefix = 'animate__') =>
     // We create a Promise and return it
     new Promise((resolve, reject) => {
         const animationName = `${prefix}${animation}`;
         //const node = document.querySelector(element);
         const $elem = $(element)
         // $($elem).removeClass('hidden-load')
-		//node.classList.add(`${prefix}animated`, animationName);
-		if(!speed){
-			$elem.addClass(`${prefix}animated ${animationName}`).one(animationEnd, function() {
-            	$(this).removeClass(`${prefix}animated ${animationName}`)
-            	resolve('Animation ended');
-        	})
-		}else{
-			$elem.addClass(`${prefix}animated ${animationName} ${prefix}${speed}`).one(animationEnd, function() {
-            	$(this).removeClass(`${prefix}animated ${animationName} ${prefix}${speed}`)
-            	resolve('Animation ended');
-        	})
-		}
+        //node.classList.add(`${prefix}animated`, animationName);
+        var classConstructor = `${prefix}animated ${animationName}`
+        if(addExtraClass)classConstructor+=` ${addExtraClass}`
+        if(speed)classConstructor+=` ${prefix}${speed}`
+        //$(this).removeClass(addExtraClass)
+        
+        $elem.addClass(classConstructor).one(animationEnd, function() {
+            if(removeExtraClass)classConstructor+=` ${removeExtraClass}`
+            $(this).removeClass(classConstructor)
+            resolve('Animation ended');
+        })
         
         // When the animation ends, we clean the classes and resolve the Promise
 
@@ -98,5 +97,43 @@ const animateCSS = (element, animation,speed, prefix = 'animate__') =>
 /*////////////////////////////////////////
 //////////////
 */////////////////////////////////////////
+
+// Element to move, time in ms to animate
+function scrollTo(element, duration) {
+    var e = document.documentElement;
+    if (e.scrollTop === 0) {
+      var t = e.scrollTop;
+      ++e.scrollTop;
+      e = t + 1 === e.scrollTop-- ? e : document.body;
+    }
+    scrollToC(e, e.scrollTop, element, duration);
+  }
+  
+  // Element to move, element or px from, element or px to, time in ms to animate
+  function scrollToC(element, from, to, duration) {
+    if (duration <= 0) return;
+    if (typeof from === "object") from = from.offsetTop;
+    if (typeof to === "object") to = to.offsetTop;
+  
+    scrollToX(element, from, to, 0, 1 / duration, 20, easeOutCuaic);
+  }
+  
+  function scrollToX(element, xFrom, xTo, t01, speed, step, motion) {
+    if (t01 < 0 || t01 > 1 || speed <= 0) {
+      element.scrollTop = xTo;
+      return;
+    }
+    element.scrollTop = xFrom - (xFrom - xTo) * motion(t01);
+    t01 += speed * step;
+  
+    setTimeout(function() {
+      scrollToX(element, xFrom, xTo, t01, speed, step, motion);
+    }, step);
+  }
+  
+  function easeOutCuaic(t) {
+    t--;
+    return t * t * t + 1;
+  }
 /////////////////////////////////////////
 //////////////////////////////////////////
