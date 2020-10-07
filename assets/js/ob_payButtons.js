e6934abe93f0cbb3127fe963985ff60506695fac
obisims <33389230+obisims@users.noreply.github.com>
2020-10-08 06:30:13 +1100

    forgot the url... wow

--- assets/js/ob_payButtons.js
+++ assets/js/ob_payButtons.js
@@ -553,7 +553,7 @@ function poliPay_initiateTransaction(settings){
      //var authKey = 
       $.ajax({
             type: 'POST',
-            url: 'url',
+            url: invoiceSettings.checkouts['Direct Debit'].api+'/v2/Transaction/Initiate',
             dataType: 'json',
             //whatever you need
             data:JSON.stringify(poliPayOptions),
