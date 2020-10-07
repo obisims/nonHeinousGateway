7c69de9c25dba924345fdc1dbfc40cfd8c1f6b03
obisims <33389230+obisims@users.noreply.github.com>
2020-10-08 06:48:31 +1100

    Update ob_payButtons.js

--- assets/js/ob_payButtons.js
+++ assets/js/ob_payButtons.js
@@ -554,7 +554,8 @@ function poliPay_initiateTransaction(settings){
       $.ajax({
             type: 'POST',
             url: invoiceSettings.checkouts['Direct Debit'].api+'/v2/Transaction/Initiate',
-            dataType: 'jsonp',
+            dataType: 'json',
+            crossDomain: true,
             //whatever you need
             data:JSON.stringify(poliPayOptions),
             headers: {
