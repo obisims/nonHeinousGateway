6dbfb98edf0f02035ab3795e678751ad5939e2bb
obisims <33389230+obisims@users.noreply.github.com>
2020-10-11 04:11:57 +1100

    yeah, thought i would need that

--- assets/js/ob_api.js
+++ assets/js/ob_api.js
@@ -1,7 +1,7 @@
 function ob_api(inv){
     // v8 // ob_api / obApi_invoices 
     if(!Number(inv))inv = inv.split('-')[1];//handle raw num and prefix
-    //inv = new Number(inv)
+    inv = new Number(inv)
     var obi_api_apiUrl = 'https://script.google.com/macros/s/AKfycbxhNWLM3sijTGy-L_-YrkVINYblPMdyaFlnnEbleFYf-apslLgs/exec'
     if(inv)inv = obi_api_apiUrl+'?'+'inv='+inv//get just this inv
     $.getJSON(obi_api_apiUrl, function(data) {
