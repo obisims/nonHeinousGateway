9301d649e0507c09cd469020369f3a8c2e86a163
obisims <33389230+obisims@users.noreply.github.com>
2020-10-12 21:08:37 +1100

    Update ob_api.js

--- assets/js/ob_api.js
+++ assets/js/ob_api.js
@@ -1,14 +1,15 @@
 function ob_api(inv){
+
     console.log('[ob_api]','inv',inv)
     // v8 // ob_api / obApi_invoices 
     //(urlParams.inv).split('-')[1]
+    var invNum = new Number(inv)
+    console.log('[ob_api]','invNum',invNum)
+    if(Number.isNaN(invNum))invNum = new Number(inv.split('-')[1]);//handle raw num and prefix
+    //console.log('[ob_api]','Number(invNum)',invNum)
     
-    if(Number.isNaN(new Number(inv)))inv = inv.split('-')[1];//handle raw num and prefix
-    console.log('[ob_api]','inv',inv)
-    inv = new Number(inv)
-    console.log('[ob_api]','Number(inv)',inv)
     var obi_api_apiUrl = 'https://script.google.com/macros/s/AKfycbxhNWLM3sijTGy-L_-YrkVINYblPMdyaFlnnEbleFYf-apslLgs/exec'
-    if(inv)obi_api_apiUrl = obi_api_apiUrl+'?'+'inv='+inv//get just this inv
+    if(inv)obi_api_apiUrl = obi_api_apiUrl+'?'+'inv='+invNum//get just this inv
     $.getJSON(obi_api_apiUrl, function(data) {
         console.log('[ob_api]','invoice data',data)
         obiAPI_params = data
